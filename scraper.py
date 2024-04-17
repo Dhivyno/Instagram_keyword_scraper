4c47dda9fe5e10a364109a30e57c180db34e1e90
Dhivyesh K <dhivyno@gmail.com>
2024-04-17 11:56:28 +0800

    Add scraper

---
+++ scraper.py
@@ -0,0 +1,52 @@
+
+
+print("""
+---------------------------------------------------------------------------------
+INSTRUCTIONS TO USE:
+      
+Input your username, password, target account and keyword or keysentence when prompted to search for posts from target account that contain keyword or keysentence.
+      
+It will print the index of each post and 
+---------------------------------------------------------------------------------
+""")
+username = input("Enter the username of your account:  ")
+password = input("Enter the password of your account:  ")
+target = input("Enter the username of the target account:  ")
+keyword = input("Enter the keyword or sentence you are looking for in the target account's posts:  ")
+
+
+import instaloader
+import pandas as pd
+
+# Create an instance of Instaloader class
+bot = instaloader.Instaloader()
+
+try:
+    bot.login(user=username,passwd=password)
+except:
+    print("ERROR LOGGING INTO ACCOUNT")
+
+
+ 
+# Loading a profile from an Instagram handle
+profile = instaloader.Profile.from_username(bot.context, target)
+
+
+ 
+# Retrieving all posts in an object
+posts = profile.get_posts()
+counter = 0
+ 
+# Iterating and downloading all the individual posts
+for index, post in enumerate(posts, 1):
+    try:
+        if keyword in post.caption:
+            print(index, counter, post.url)
+            bot.download_post(post, target=f"{profile.username}_{index}")
+            counter += 1
+        else:
+            print(index)
+    except:
+        continue
+
+print(str(counter) + " posts found with keyword: " + keyword)
